cedc7586fd5d5ff67d590e54008c35f1d89f8448
Chizuui <desckun@gmail.com>
2025-08-29 08:59:35 +0700

    adasdasdas

--- basic-programming/2025_semester_1/29_aug/test.py
+++ basic-programming/2025_semester_1/29_aug/test.py
@@ -1,8 +1,10 @@
 name = input("Your name is: ")
 age = input("Your age is: ")
+matakuliah = "aaa"
+var1, var2, var3 = 5, "test2", 3.14
 
 #print("Hello " + name + ", you are " + age + " years old.")
-print(f"Hello {name}.")
+print("Hello {name}.")
 
 print("Hello World")
 
@@ -12,3 +14,9 @@ if age > 30:
     print("Umur Mu Diatas 30 Tahun")
 else:
     print("Umur Mu Masih Di Bawah 30 Tahun")
+
+print(var1, var2, var3)
+
+print (matakuliah)
+del (matakuliah)
+print ("Matakuliah telah dihapus")
