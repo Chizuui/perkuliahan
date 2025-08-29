d44324ab8d257f097b1d3888aea417f0f115a4e4
Chizuui <desckun@gmail.com>
2025-08-29 08:26:51 +0700

    if else test

--- basic-programming/2025_semester_1/29_aug/test.py
+++ basic-programming/2025_semester_1/29_aug/test.py
@@ -2,3 +2,12 @@ name = input("Your name is: ")
 age = input("Your age is: ")
 
 print("Hello " + name + ", you are " + age + " years old.")
+
+print("Hello World")
+
+umur = 25
+
+if (umur > 30):
+    print ("Umur Mu Diatas 30 Tahun")
+else:
+    print ("Umur Mu Masih Di Bawah 30 Tahun")
