ac68f0b9286ff478c180b165213bc3b69237941f
ChizzUI <desckun@gmail.com>
2025-09-12 16:05:08 +0700

    calc???

--- simple calc2.py
+++ simple calc2.py
@@ -1,9 +1,7 @@
-# Minta input dari pengguna
 angka1 = float(input("Masukkan angka pertama: "))
 operator = input("Masukkan operator (+, -, *, /): ")
 angka2 = float(input("Masukkan angka kedua: "))
 
-# Lakukan pengecekan dan perhitungan berdasarkan operator yang dimasukkan
 if operator == '+':
     hasil = angka1 + angka2
     print(f"Hasil: {angka1} + {angka2} = {hasil}")
