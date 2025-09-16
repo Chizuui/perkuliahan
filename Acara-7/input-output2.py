ce724595a37da04a77ce36143fa92765bd342626
ChizzUI <desckun@gmail.com>
2025-09-16 11:02:22 +0700

    a

---
+++ Acara-7/input-output2.py
@@ -0,0 +1,5 @@
+nama = input("Masukkan nama Anda: ")
+umur = int(input("Masukkan umur Anda: "))
+tinggi = float(input("Masukkan tinggi badan Anda (dalam cm): "))
+
+print ("Halo %s, Umur kamu %i tahun dan tinggi badambu %f cm." % (nama, umur, tinggi))
---
+++ a.py
@@ -0,0 +1,7 @@
+#nama = input("Masukkan nama Anda: ")
+#usia = input("Masukkan usia Anda: ")
+#
+#print("Selamat datang, ", nama, ". Anda berusia", usia, "tahun.")
+
+angka = input("masukan sebuah angka : ")
+print(n+10)
