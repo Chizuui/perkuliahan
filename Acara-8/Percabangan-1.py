83899aef4e77df76c3f58512ad17b06acd89b0aa
ChizzUI <desckun@gmail.com>
2025-09-23 07:37:35 +0700

    acara 8

---
+++ Acara-8/Percabangan-1.py
@@ -0,0 +1,11 @@
+#if 
+angka = int(input("Masukkan angka: "))
+
+if angka > 5:
+    print("angka lebih besar dari 5")
+
+if angka < 5:
+    print("angka lebih kecil dari 5")
+
+if angka == 5:
+    print("angka sama dengan 5")
