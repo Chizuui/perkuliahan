df449c56e18d593e13553e6ae4a75e430e0fc044
ChizzUI <desckun@gmail.com>
2025-09-26 07:30:08 +0700

    1

---
+++ Acara-10/lopping.py
@@ -0,0 +1,12 @@
+jumlah = 1
+
+while jumlah <= 10:
+    print (jumlah)
+    jumlah = jumlah +1
+    
+print("Selesai program 1")
+
+while jumlah > 0:
+    print (jumlah)
+    jumlah -= 1
+print("Selesai")
