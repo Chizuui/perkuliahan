7079ff3eb6301004812016ee9d066dac56e23716
Chizuui <desckun@gmail.com>
2025-09-27 14:54:58 +0700

    aaa

---
+++ Acara-10/loop.py
@@ -0,0 +1,7 @@
+angka = [1,2,3,4,5,6,7,8,9,10]
+jumlah = 0
+
+for i in angka:
+    jumlah = jumlah + i
+
+print ('jumlah =', jumlah)
--- Acara-10/while-true.py
+++ Acara-10/while-true.py
@@ -3,7 +3,11 @@ while True :
     print(angka)
     angka = angka + 1
 
-    if angka > 5:
-        print("break")
+    if angka == 2:
+        print("skip")
+        continue
+    
+    if angka == 5:
+        print ("break")
         break
 print("selesai")
