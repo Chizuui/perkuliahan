3811e85ce7eae1a515dd91d6804dd8cb64b8d07f
Chizuui <desckun@gmail.com>
2025-09-30 10:36:29 +0700

    aaaa

--- Acara-11/list1.py
+++ Acara-11/list1.py
@@ -16,8 +16,24 @@ ListNasted = ["Mahasiswa", ["Dian", 17, 160.5],"Lulus"]
 ListGabugan = [ListKata, listAngka]
 
 # mencetak hasil
-print (listAngka)
-print (ListKata)
-print (ListCampuran)
-print (ListNasted)
-print (ListGabugan)
+#print (listAngka)
+#print (ListKata)
+#print (ListCampuran)
+#print (ListNasted)
+#print (ListGabugan)
+
+# Mencetak indeks dari list
+#print (ListNasted[0])
+#print (ListNasted[1])
+#print (ListNasted[1][0])
+#print (ListNasted[1][1])
+#print (ListNasted[1][2])
+#print (ListNasted[2])
+
+# Membuat list kosong
+ListKosong = []
+
+kata = "WORKSHOP"
+angka = 12345678
+print (kata[5])
+print (angka[5])
