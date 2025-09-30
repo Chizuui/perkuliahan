ae066429de629d753152122265a00480a81899df
Chizuui <desckun@gmail.com>
2025-09-30 07:41:45 +0700

    1

---
+++ Acara-11/list1.py
@@ -0,0 +1,23 @@
+# membuat list kosong
+ListKosong = []
+
+# membuat list berisi 1 item
+List_1_Item = ['satuItem']
+
+# membuat list dengan banyak item dengan satu tipe data
+ListKata = ["mangga", "jambu", "apel", "anggur "]
+listAngka = [23, 11, 94] 
+
+# membuat list dengan berbagai tipe data
+ListCampuran = [23, "Novemver", 94]
+
+# Membuat list berisi list ( bercabang)
+ListNasted = ["Mahasiswa", ["Dian", 17, 160.5],"Lulus"]
+ListGabugan = [ListKata, listAngka]
+
+# mencetak hasil
+print (listAngka)
+print (ListKata)
+print (ListCampuran)
+print (ListNasted)
+print (ListGabugan)
