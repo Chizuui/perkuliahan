13fcf8b77b0572784b5231019adda6ea75873da1
Chizuui <desckun@gmail.com>
2025-10-07 07:16:39 +0700

    Refactor SequenceUnpacking.py to encapsulate functionality in finchion1() and add print statements for tuple unpacking Add tupleFunc.py to demonstrate tuple operations including max, min, and length

--- Acara-12/SequenceUnpacking.py
+++ Acara-12/SequenceUnpacking.py
@@ -1,8 +1,13 @@
-tuple1 = (1,2,3)
-tuple2 = ('satu', 'dua', 'tiga')
-tuple3 = ('andi', 23,68.5)
-tuple4 = (tuple1, tuple2, tuple3)
+def finchion1():
+    tuple1 = (1, 2, 3)
+    tuple2 = ('satu', 'dua', 'tiga')
+    tuple3 = ('andi', 23, 68.5)
+    tuple4 = (tuple1, tuple2, tuple3)
 
-print (max(tuple1))
-print (min(tuple1))
-print (len(tuple1))
+    nama, usia, berat = tuple3
+
+    print ('nama : ', nama)
+    print ('usia : ', usia)
+    print ('berat : ', berat)
+
+finchion1()
