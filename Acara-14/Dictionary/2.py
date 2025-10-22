f06b59678963238ed5946712c46e893dcaec90a6
Chizuui <desckun@gmail.com>
2025-10-22 16:48:16 +0700

    git cache cleared

--- Acara-14/Dictionary/2.py
+++ Acara-14/Dictionary/2.py
@@ -10,17 +10,56 @@ mahasiswa = {
     }
 }
 
-# print(mahasiswa)
+for key in sorted(mahasiswa):
+    print('%s: %s' % (key, mahasiswa[key]))
+    
+# print('Jumlah key pada variable siswa :', len(mahasiswa))
 
-#mengakses item pada dictionary
-#menggunakan get()
-print('Nama Mahasiswa:', mahasiswa.get("nama"))
+# print('Apakah dictionary mahasiswa memiliki key nama?')
+# print('nama' in mahasiswa)
 
-# menggunakan kurung siku []
-print ('Umur Mahasiswa:', mahasiswa["umur"])
+# print('\nApakah variable siswa TIDAK memiliki key status?')
+# print('status' not in mahasiswa)
 
-# menggunakan fungsi berantai untuk dictionary bertingkat
-print('Jurusan Mahasiswa:', mahasiswa.get("kuliah").get("jurusan"))
 
-# menggunakan kurung siku di kedua dua nya
-print('Prodi Mahasiswa:', mahasiswa["kuliah"]["prodi"])
+# # menghaus item
+# del mahasiswa ['nama']
+# print('Nama Mahasiswa:', mahasiswa.get("nama"))
+
+# # menghapus menggunakan pop
+# mahasiswa.pop ('umur')
+# print('Umur Mahasiswa:', mahasiswa.get("umur"))
+
+# # menampilkan nilai kembalian data yang saudah dihapus
+# gender = mahasiswa.pop('Gender')
+# print('jenis kelamin mahasiswa:', gender)
+
+# # mencetak key alamat sekaligus cek
+# print('Alamat:', mahasiswa.get("alamat"))
+
+# # menambahkan key alamat ke dalam dict mahasiswa 
+# mahasiswa["alamat"] = "jember"
+
+# # mencetak hasil 
+# print('Mahasiswa {} beralamat di {}'.format(
+#     mahasiswa.get('nama'),
+#     mahasiswa.get('alamat')
+# ))
+
+# # mengubah nilai item
+# print('Nama Awal:', mahasiswa.get("nama"))
+# mahasiswa["nama"] = "Budi Santoso"
+# print('Nama Setelah Diubah:', mahasiswa.get("nama"))
+
+# #mengakses item pada dictionary
+# #menggunakan get()
+# print('Nama Mahasiswa:', mahasiswa.get("nama"))
+
+# # menggunakan kurung siku []
+# print ('Umur Mahasiswa:', mahasiswa["umur"])
+
+# # menggunakan fungsi berantai untuk dictionary bertingkat
+# print('Jurusan Mahasiswa:', mahasiswa.get("kuliah").get("jurusan"))
+
+# # menggunakan kurung siku di kedua dua nya
+# print('Prodi Mahasiswa:', mahasiswa["kuliah"]["prodi"])
